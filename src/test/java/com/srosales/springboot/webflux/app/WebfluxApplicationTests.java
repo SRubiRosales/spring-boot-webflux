d171aac314357c1e528903399f7de5a24d3bd83a
SRubiRosales <srosales@bancobase.com>
2023-10-31 17:23:45 -0600

    Proyecto nuevo

---
+++ src/test/java/com/srosales/springboot/webflux/app/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srosales.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
