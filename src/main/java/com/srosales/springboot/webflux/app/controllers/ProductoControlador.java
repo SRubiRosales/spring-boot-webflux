6e651fd35a187de06511e59892cb3fc6e8df93fe
SRubiRosales <srosales@bancobase.com>
2023-11-30 17:14:45 -0600

    Observador de log agregado
    Map con upperscase de productos

--- src/main/java/com/srosales/springboot/webflux/app/controllers/ProductoControlador.java
+++ src/main/java/com/srosales/springboot/webflux/app/controllers/ProductoControlador.java
@@ -2,6 +2,8 @@ package com.srosales.springboot.webflux.app.controllers;
 
 import com.srosales.springboot.webflux.app.modelos.dao.ProductoDao;
 import com.srosales.springboot.webflux.app.modelos.documentos.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,9 +15,16 @@ public class ProductoControlador {
     @Autowired
     private ProductoDao dao;
 
+    private static final Logger log = LoggerFactory.getLogger(ProductoControlador.class);
+
     @GetMapping({"/listar", "/"})
     public String listar(Model model) {
-        Flux<Producto> productos = dao.findAll();
+        Flux<Producto> productos = dao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+
+        productos.subscribe(p -> log.info(p.getNombre()));
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Lista de productos");
         return "listar";
