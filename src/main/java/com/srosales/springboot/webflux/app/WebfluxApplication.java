a5ecd63118871e73cc7e5bfec744513cd275b2b6
SRubiRosales <srosales@bancobase.com>
2024-06-19 17:03:17 -0600

    Agregar categorias de ejemplo
    Persistencia en documento Mongo

--- src/main/java/com/srosales/springboot/webflux/app/WebfluxApplication.java
+++ src/main/java/com/srosales/springboot/webflux/app/WebfluxApplication.java
@@ -1,7 +1,8 @@
 package com.srosales.springboot.webflux.app;
 
-import com.srosales.springboot.webflux.app.modelos.dao.ProductoDao;
+import com.srosales.springboot.webflux.app.modelos.documentos.Categoria;
 import com.srosales.springboot.webflux.app.modelos.documentos.Producto;
+import com.srosales.springboot.webflux.app.modelos.servicios.ProductoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ import java.util.Date;
 public class WebfluxApplication implements CommandLineRunner {
 
 	@Autowired
-	private ProductoDao dao;
+	private ProductoServicio servicio;
 
 	@Autowired
 	private ReactiveMongoTemplate mongoTemplate;
@@ -32,22 +33,35 @@ public class WebfluxApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		//Elimina los datos del collection al finalizar
 		mongoTemplate.dropCollection("productos").subscribe();
+		mongoTemplate.dropCollection("categorias").subscribe();
 
-		Flux.just(
-				new Producto("TV Panasonic Pantalla LCD", 456.89),
-				new Producto("Sony Camara HD Digital", 177.89),
-				new Producto("Apple iPod", 46.89),
-				new Producto("Sony Notebook", 846.89),
-				new Producto("Hewlett Packard Multifuncional", 200.89),
-				new Producto("Bianchi Bicicleta", 70.89),
-				new Producto("HP Notebook Omen 17", 2500.89),
-				new Producto("Mica Comoda 5 cajones", 150.89),
-				new Producto("TV Sony Bravia 4K Ultra HD", 2255.89)
-			)
-			.flatMap(producto -> {
-				producto.setCreateAt(new Date());
-				return dao.save(producto);
+		Categoria electronica = new Categoria("Electronica");
+		Categoria deporte = new Categoria("Deporte");
+		Categoria computacion = new Categoria("Computacion");
+		Categoria muebles = new Categoria("Muebles");
+
+		Flux.just(electronica, deporte, computacion, muebles)
+			.flatMap(servicio::guardarCategoria)
+			.doOnNext(categoria ->  {
+				log.info("Categoria creada: " + categoria.getNombre() + categoria.getId());
 			})
+			.thenMany(
+				Flux.just(
+					new Producto("TV Panasonic Pantalla LCD", 456.89, electronica),
+					new Producto("Sony Camara HD Digital", 177.89, electronica),
+					new Producto("Apple iPod", 46.89, electronica),
+					new Producto("Sony Notebook", 846.89, computacion),
+					new Producto("Hewlett Packard Multifuncional", 200.89, computacion),
+					new Producto("Bianchi Bicicleta", 70.89, deporte),
+					new Producto("HP Notebook Omen 17", 2500.89, computacion),
+					new Producto("Mica Comoda 5 cajones", 150.89, muebles),
+					new Producto("TV Sony Bravia 4K Ultra HD", 2255.89, electronica)
+				)
+				.flatMap(producto -> {
+					producto.setCreateAt(new Date());
+					return servicio.guardar(producto);
+				})
+			)
 			.subscribe(productoMono -> log.info("Insert: " + productoMono.getId() + " " + productoMono.getNombre()));
 	}
 }
